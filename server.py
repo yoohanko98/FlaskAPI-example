be812263e2a9bc58a33179b89033508112471098
yoohanko98 <yoohanko98@gmail.com>
2022-02-21 21:21:06 -0500

    Create placeholder routes for API

--- server.py
+++ server.py
@@ -12,6 +12,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
 
 # configure sqlite3 to encorce foreign key constraints
+# in order for someone to make a valid blog post, they must be in the "user" table
 @event.listens_for(Engine, "connect")
 def _set_sqlite_pragma(dbapi_connection, connection_record):
     if isinstance(dbapi_connection, SQLite3Connection):
@@ -22,6 +23,10 @@ def _set_sqlite_pragma(dbapi_connection, connection_record):
 db = SQLAlchemy(app)
 now = datetime.now()
 
+
+# Object Relational Mapper (ORM) allows you to interact with a database in an object oriented way
+# We can create a class that is representative of a table in a database
+
 # models
 class User(db.Model):
     __tablename__ = "user"
@@ -31,3 +36,51 @@ class User(db.Model):
     address = db.Column(db.String(200))
     phone = db.Column(db.String(50))
     posts = db.relationship("BlogPost")
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_post"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(200))
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+# routes
+@app.route("/user", methods=["POSTS"])
+def create_user():
+    pass
+
+@app.route("/user/descending_id", methods=["GET"])
+def get_all_users_descending():
+    pass
+
+@app.route("/user/ascending_id", methods=["GET"])
+def get_all_users_ascending():
+    pass
+
+@app.route("/user/<user_id>", methods=["GET"])
+def get_one_user(user_id):
+    pass
+
+@app.route("/user/<user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    pass
+
+@app.route("/blog_post/<user_id>", methods=["POST"])
+def create_blog_post(user_id):
+    pass
+
+@app.route("/user/<user_id>", methods=["GET"])
+def get_all_blog_posts(user_id):
+    pass
+
+@app.route("/blog_post/<blog_post_id>", methods=["GET"])
+def get_one_blog_post(user_id):
+    pass
+
+@app.route("/blog_post/<blog_post_id>", methods=["DELETE"])
+def delete_blog_post(user_id):
+    pass
+
+if __name__ == "__main__":
+    app.run(debug=True)
